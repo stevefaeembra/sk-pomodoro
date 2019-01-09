d7206e5a70901df7bc4bd500f189c74e40ec5c43
Steven Kay <stevendkay@gmail.com>
2019-01-09 20:41:19 +0000

    change to use port 7666 (POMO) - unless container e.g. Heroku chooses otherwise...

--- server/app.js
+++ server/app.js
@@ -6,7 +6,9 @@ const publicPath = path.join(__dirname, '../client/public');
 
 app.use('/', express.static(publicPath));
 
-const port = process.env.PORT || 3000
+// port 7666 so it won't clash if you're writing node apps ;-)
+// 7666 from the old phone code for POMO (7-6-6-6)
+const port = process.env.PORT || 7666
 
 app.get('/', (req,res) => {
   res.sendFile(path.join(__dirname, "index.html"));
